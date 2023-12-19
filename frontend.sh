e2ed5e4874705353e1913390dd76051a790e37f8
Veena <sriramadasveena@gmail.com>
2023-12-19 20:32:02 +0530

    <frontend servers check>

--- frontend.sh
+++ frontend.sh
@@ -3,9 +3,9 @@ systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+#cd /usr/share/nginx/html
+#unzip /tmp/frontend.zip
 
-vim /etc/nginx/default.d/roboshop.conf
+#vim /etc/nginx/default.d/roboshop.conf
 
-systemctl restart nginx
+#systemctl restart nginx
